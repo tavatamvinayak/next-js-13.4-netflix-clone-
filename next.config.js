98345ee2e1a82c754a53286ee5a488f6010f8ab1
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2023-06-01 13:53:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,7 @@
 const nextConfig = {
     images:{
         domains:["image.tmdb.org"]
-    },
-    // // imp start
-
-    webpack: (config, { isServer }) => {
-
-        if (!isServer) {
-            config.target = 'electron-renderer';
-            config.node = {
-                __dirname: true,
-            }
-        }
-        return config;
-    },
-
-    /// // imp end
+    }
 }
 
 module.exports = nextConfig
